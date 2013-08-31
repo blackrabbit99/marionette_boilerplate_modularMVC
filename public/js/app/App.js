4ef665e2b26c61f8d58e190f72165030e6dfa816
Artem Zakharchenko <artem.zakharchenko@dev-pro.net>
2013-08-31 18:38:14 +0300

    configure module routes

--- public/js/app/App.js
+++ public/js/app/App.js
@@ -1,6 +1,8 @@
 define(['jquery', 'backbone', 'marionette', 'underscore', 'handlebars'],
     function ($, Backbone, Marionette, _, Handlebars) {
         var App = new Backbone.Marionette.Application();
+
+        window.Picsio = App;
         //Organize Application into regions corresponding to DOM elements
         //Regions can contain views, Layouts, or subregions nested as necessary
         App.addRegions({
--- public/js/app/main/routes/routes.js
+++ public/js/app/main/routes/routes.js
@@ -1,11 +1,14 @@
 define(["App"], function(App) {
+	
 	App.routes = App.routes || {};
 	var routes = {
+		namespace: "main",
+		root: true,
 		appRoutes: {
 			"": "index"
 		}
 	}
 
-	App.routes = routes;
+	App.routes[routes.namespace] = routes;
 
 });
